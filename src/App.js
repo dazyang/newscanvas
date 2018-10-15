730856e1c2f39d39b6fd5056ebfadf81fa2ccee3
dazyang <dazyang@protonmail.com>
2018-10-15 15:02:00 +1100

    installed material ui/deleted react logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// import Card from '@material-ui/core/Card';
 
 class App extends Component {
   render() {
